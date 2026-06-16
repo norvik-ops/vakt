6552edc2aa943f0c087f647004fbd27bd2045b75
NorvikOps CI <ci@norvikops.de>
2026-06-16 07:23:58 +0000

    sync: update from vakt-app@f95143b21b69d3aed6657823ad9fef311a8d5e69

--- backend/internal/modules/vaktcomply/service_sprint86_mappings.go
+++ backend/internal/modules/vaktcomply/service_sprint86_mappings.go
@@ -11,20 +11,20 @@ var der4CrossMappings = []struct {
 	src, srcCode, tgt, tgtCode, mtype string
 }{
 	// DER.4 ↔ ISO 27001:2022
-	{"BSI", "BSI-DER.4.A1", "ISO27001", "A.5.29", "equivalent"}, // Notfallhandbuch = Kontinuitätsverfahren
-	{"BSI", "BSI-DER.4.A4", "ISO27001", "A.5.29", "equivalent"}, // BIA = Planung ICT-Readiness
-	{"BSI", "BSI-DER.4.A4", "ISO27001", "A.8.13", "related"},    // BIA → Backup-Informationen
-	{"BSI", "BSI-DER.4.A5", "ISO27001", "A.5.29", "equivalent"}, // Notfallkonzept = Umsetzung
-	{"BSI", "BSI-DER.4.A5", "ISO27001", "A.8.14", "related"},    // WAP → Redundanz IT
-	{"BSI", "BSI-DER.4.A6", "ISO27001", "A.5.30", "equivalent"}, // Übungen = ICT readiness for BC
-	{"BSI", "BSI-DER.4.A8", "ISO27001", "A.5.29", "related"},    // Alarmierungsplan
+	{"BSI", "BSI-DER.4.A1", "ISO27001", "A.5.29", "equivalent"},  // Notfallhandbuch = Kontinuitätsverfahren
+	{"BSI", "BSI-DER.4.A4", "ISO27001", "A.5.29", "equivalent"},  // BIA = Planung ICT-Readiness
+	{"BSI", "BSI-DER.4.A4", "ISO27001", "A.8.13", "informative"}, // BIA → Backup-Informationen
+	{"BSI", "BSI-DER.4.A5", "ISO27001", "A.5.29", "equivalent"},  // Notfallkonzept = Umsetzung
+	{"BSI", "BSI-DER.4.A5", "ISO27001", "A.8.14", "informative"}, // WAP → Redundanz IT
+	{"BSI", "BSI-DER.4.A6", "ISO27001", "A.5.30", "equivalent"},  // Übungen = ICT readiness for BC
+	{"BSI", "BSI-DER.4.A8", "ISO27001", "A.5.29", "informative"}, // Alarmierungsplan
 
 	// DER.4 ↔ NIS2
 	{"BSI", "BSI-DER.4.A1", "NIS2", "NIS2-C.1", "equivalent"}, // ICT readiness / BCM
 	{"BSI", "BSI-DER.4.A4", "NIS2", "NIS2-B.5", "equivalent"}, // Notfallmanagementplan
-	{"BSI", "BSI-DER.4.A5", "NIS2", "NIS2-C.1", "related"},
+	{"BSI", "BSI-DER.4.A5", "NIS2", "NIS2-C.1", "informative"},
 
 	// DER.4 ↔ DORA (Finanzsektor)
-	{"BSI", "BSI-DER.4.A5", "DORA", "DORA-S.3", "related"}, // ICT Recovery Plans
-	{"BSI", "BSI-DER.4.A6", "DORA", "DORA-S.4", "related"}, // Testing
+	{"BSI", "BSI-DER.4.A5", "DORA", "DORA-S.3", "informative"}, // ICT Recovery Plans
+	{"BSI", "BSI-DER.4.A6", "DORA", "DORA-S.4", "informative"}, // Testing
 }
